135d728060effff629d6d5c518426229dc7c7a9d
James Richardson <me+github@james.richardson.name>
2011-07-17 20:38:49 +0100

    - removed references to SimpleStats

--- name/richardson/james/dimensiondoor/DimensionDoorWorldListener.java
+++ name/richardson/james/dimensiondoor/DimensionDoorWorldListener.java
@@ -3,12 +3,12 @@ Copyright 2011 James Richardson.
 
 This file is part of DimensionDoor.
 
-SimpleStats is free software: you can redistribute it and/or modify
+DimensionDoor is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-SimpleStats is distributed in the hope that it will be useful,
+DimensionDoor is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
@@ -16,6 +16,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with DimensionDoor.  If not, see <http://www.gnu.org/licenses/>.
 */
+
 
 package name.richardson.james.dimensiondoor;
 
