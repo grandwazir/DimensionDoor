a78cfaf7cdfc71fd02268c9c3217952bc89403d0
James Richardson <me+github@james.richardson.name>
2011-12-18 01:46:23 +0000

    amended copyright notices

--- name/richardson/james/dimensiondoor/database/WorldRecordHandler.java
+++ name/richardson/james/dimensiondoor/database/WorldRecordHandler.java
@@ -1,20 +1,20 @@
 /*******************************************************************************
  * Copyright (c) 2011 James Richardson.
  * 
- * ReservationRecordHandler.java is part of Reservation.
+ * WorldRecordHandler.java is part of DimensionDoor.
  * 
- * Reservation is free software: you can redistribute it and/or modify it under
- * the terms of the GNU General Public License as published by the Free Software
- * Foundation, either version 3 of the License, or (at your option) any later
- * version.
+ * DimensionDoor is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
  * 
- * Reservation is distributed in the hope that it will be useful, but WITHOUT
+ * DimensionDoor is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
  * details.
  * 
  * You should have received a copy of the GNU General Public License along with
- * Reservation. If not, see <http://www.gnu.org/licenses/>.
+ * DimensionDoor. If not, see <http://www.gnu.org/licenses/>.
  ******************************************************************************/
 
 package name.richardson.james.dimensiondoor.database;
--- name/richardson/james/dimensiondoor/util/Handler.java
+++ name/richardson/james/dimensiondoor/util/Handler.java
@@ -1,19 +1,20 @@
 /*******************************************************************************
  * Copyright (c) 2011 James Richardson.
  * 
- * Handler.java is part of jChat.
+ * Handler.java is part of DimensionDoor.
  * 
- * jChat is free software: you can redistribute it and/or modify it under the
- * terms of the GNU General Public License as published by the Free Software
- * Foundation, either version 3 of the License, or (at your option) any later
- * version.
+ * DimensionDoor is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
  * 
- * jChat is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
- * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * DimensionDoor is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
  * 
  * You should have received a copy of the GNU General Public License along with
- * jChat. If not, see <http://www.gnu.org/licenses/>.
+ * DimensionDoor. If not, see <http://www.gnu.org/licenses/>.
  ******************************************************************************/
 
 package name.richardson.james.dimensiondoor.util;
