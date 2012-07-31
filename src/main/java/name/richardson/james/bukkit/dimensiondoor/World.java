5181b82263b70c5d5aaecc46c0bba110b20ccd4f
James Richardson <me+github@james.richardson.name>
2012-07-31 15:34:39 +0100

    add documentation

--- src/main/java/name/richardson/james/bukkit/dimensiondoor/World.java
+++ src/main/java/name/richardson/james/bukkit/dimensiondoor/World.java
@@ -1,9 +1,7 @@
 package name.richardson.james.bukkit.dimensiondoor;
 
 import java.io.Serializable;
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
@@ -32,8 +30,15 @@ import name.richardson.james.bukkit.utilities.localisation.Localised;
 
 public class World extends Localised implements Serializable, Listener {
 
+  /** The Constant serialVersionUID. */
   private static final long serialVersionUID = 8551768503578434301L;
 
+  /**
+   * Deserialize a world into a new object.
+   *
+   * @param map the map containing the data
+   * @return a new world object
+   */
   public static World deserialize(final Map<String, Object> map) {
     DimensionDoor plugin = (DimensionDoor) Bukkit.getServer().getPluginManager().getPlugin("DimensionDoor");
     final World world = new World(plugin, (String) map.get("world-name"));
@@ -49,74 +54,81 @@ public class World extends Localised implements Serializable, Listener {
     world.setSeed((Long) map.get("seed"));
     return world;
   }
-  
+
+  /** The logger. */
   private final Logger logger = new Logger(this.getClass());
-  
-  /** The loaded world */
+
+  /** The loaded world. */
   private org.bukkit.World world;
-  
-  /** The difficulty of the world */
+
+  /** The difficulty of the world. */
   private Difficulty difficulty = Difficulty.NORMAL;
-  
-  /** If we should keep the spawn of this world in memory */
+
+  /** If we should keep the spawn of this world in memory. */
   private boolean keepSpawnInMemory = true;
-  
-  /** If we should allow PVP in this world */
+
+  /** If we should allow PVP in this world. */
   private boolean pvp = false;
-  
-  /** If we should allow monsters to spawn in this world */
+
+  /** If we should allow monsters to spawn in this world. */
   private boolean allowMonsters = true;
-  
-  /** If we should allow animals to spawn in this world */
+
+  /** If we should allow animals to spawn in this world. */
   private boolean allowAnimals = true;
-  
-  /** The type of the world */
+
+  /** The type of the world. */
   private WorldType worldType = WorldType.NORMAL;
 
-  /** The game mode of this world */
+  /** The game mode of this world. */
   private GameMode gameMode = GameMode.SURVIVAL;
-  
-  /** The plugin */
+
+  /** The plugin. */
   private DimensionDoor plugin;
-  
-  /** The permission required to enter this world */
+
+  /** The permission required to enter this world. */
   private Permission permission;
 
-  /** The name of the world */
+  /** The name of the world. */
   private final String worldName;
 
-  /** The type of environment of this world */
+  /** The type of environment of this world. */
   private Environment environment;
 
-  /** If we are generating structures in this world */
+  /** If we are generating structures in this world. */
   private boolean generateStructures = true;
-  
-  /** The seed of this world */
+
+  /** The seed of this world. */
   private long seed;
-  
-  /** The name of the generator plugin */
-  private String generatorPluginName; 
-  
-  /** The name of the generator id */
+
+  /** The name of the generator plugin. */
+  private String generatorPluginName;
+
+  /** The name of the generator id. */
   private String generatorID;
-  
-  /** Get the unique ID for this world */
+
+  /** Get the unique ID for this world. */
   private UUID worldUUID;
-  
-  /** If chat on this world is isolated */
+
+  /** If chat on this world is isolated. */
   private boolean isolatedChat = true;
-  
-  /** Is the world enabled (automatically load on startup) */
+
+  /** Is the world enabled (automatically load on startup). */
   private boolean enabled = true;
-  
-  /** Do we listen for events or not? */
+
+  /** Do we listen for events or not?. */
   private boolean listening = true;
-  
+
+  /**
+   * Instantiates a new world and base the attributes of the world provided.
+   *
+   * @param plugin the plugin
+   * @param world the world
+   */
   public World(DimensionDoor plugin, org.bukkit.World world) {
     super(plugin);
     this.logger.debug("Initalising world object.");
     this.logger.debug(String.format("Using attributes from %s as a base.", world.getName()));
-    this.plugin = plugin; 
+    this.plugin = plugin;
     this.world = world;
     this.worldName = world.getName();
     this.allowAnimals = world.getAllowAnimals();
@@ -129,15 +141,24 @@ public class World extends Localised implements Serializable, Listener {
     this.worldUUID = world.getUID();
     this.plugin.getServer().getPluginManager().registerEvents(this, plugin);
   }
-  
+
+  /**
+   * Instantiates a new world with the default attributes.
+   *
+   * @param plugin the plugin
+   * @param worldName the world name
+   */
   public World(DimensionDoor plugin, String worldName) {
     super(plugin);
-    this.plugin = plugin; 
+    this.plugin = plugin;
     this.worldName = worldName;
     this.checkIfWorldIsLoaded();
     this.plugin.getServer().getPluginManager().registerEvents(this, plugin);
   }
-  
+
+  /**
+   * Apply world attributes.
+   */
   public void applyAttributes() {
     this.logger.debug(String.format("Applying saved attributes for %s.", this.worldName));
     world.setDifficulty(difficulty);
@@ -146,39 +167,77 @@ public class World extends Localised implements Serializable, Listener {
     world.setSpawnFlags(allowMonsters, allowAnimals);
     this.applyGameMode();
   }
-  
+
+  /**
+   * Gets the generator id.
+   *
+   * @return the generator id
+   */
   public String getGeneratorID() {
     return generatorID;
   }
-  
+
+  /**
+   * Gets the name of the world.
+   *
+   * @return the name
+   */
   public String getName() {
     return this.worldName;
   }
-  
+
+  /**
+   * Gets the UUID.
+   *
+   * @return the UUID
+   */
   public UUID getUUID() {
     return this.worldUUID;
   }
-  
+
+  /**
+   * Checks if is chat isolated.
+   *
+   * @return true, if is chat isolated
+   */
   public boolean isChatIsolated() {
     return isolatedChat;
   }
-  
+
+  /**
+   * Checks if is enabled.
+   *
+   * @return true, if is enabled
+   */
   public boolean isEnabled() {
     return enabled;
   }
-  
+
+  /**
+   * Checks if is main world.
+   *
+   * @return true, if is main world
+   */
   public boolean isMainWorld() {
     return (plugin.getServer().getWorlds().get(0) == world);
   }
-  
+
+  /**
+   * Attempt to load the world.
+   */
   public void load() {
     this.logger.debug(String.format("Loading %s into memory.", this.worldName));
     if (world != null) {
       this.getWorldCreator().createWorld();
     }
   }
-  
-  @EventHandler(priority=EventPriority.NORMAL, ignoreCancelled=true)
+
+  /**
+   * When a player has sent a chat message, check if this world is isolated and if it is prevent the message leaving the world.
+   *
+   * @param event the event
+   */
+  @EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
   public void onPlayerChat(PlayerChatEvent event) {
     if (!listening) return;
     if (this.isolatedChat == true && event.getPlayer().getWorld() == this.world) {
@@ -187,8 +246,13 @@ public class World extends Localised implements Serializable, Listener {
       event.getRecipients().addAll(this.world.getPlayers());
     }
   }
-  
-  @EventHandler(priority=EventPriority.NORMAL, ignoreCancelled=true)
+
+  /**
+   * When a player teleports check they are allowed to enter the world.
+   *
+   * @param event the event
+   */
+  @EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
   public void onPlayerTeleportEvent(PlayerTeleportEvent event) {
     if (!listening) return;
     if (event.getTo().getWorld() == this.world) {
@@ -202,8 +266,13 @@ public class World extends Localised implements Serializable, Listener {
       }
     }
   }
-  
-  @EventHandler(priority=EventPriority.MONITOR, ignoreCancelled=true)
+
+  /**
+   * When a world is loaded, set a reference to the world if it isn't present and apply attributes.
+   *
+   * @param event the event
+   */
+  @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
   public void onWorldLoaded(WorldLoadEvent event) {
     if (!listening) return;
     if (!event.getWorld().getName().equalsIgnoreCase(worldName)) return;
@@ -211,15 +280,25 @@ public class World extends Localised implements Serializable, Listener {
     if (world == null) this.world = event.getWorld();
     this.applyAttributes();
   }
-  
-  @EventHandler(priority=EventPriority.MONITOR, ignoreCancelled=true)
+
+  /**
+   * When a world is unloaded, remove the reference to the world.
+   *
+   * @param event the event
+   */
+  @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
   public void onWorldUnloaded(WorldUnloadEvent event) {
     if (!listening) return;
     if (!event.getWorld().getName().equalsIgnoreCase(worldName)) return;
     this.logger.debug(String.format("%s has unloaded, setting world to null.", this.worldName));
     this.world = null;
   }
-  
+
+  /**
+   * Serialize the world and attributes.
+   *
+   * @return the map
+   */
   public Map<String, Object> serialize() {
     this.logger.debug(String.format("Serializing %s.", world.getName()));
     final Map<String, Object> map = new HashMap<String, Object>();
@@ -236,106 +315,184 @@ public class World extends Localised implements Serializable, Listener {
     map.put("seed", seed);
     return map;
   }
-  
+
+  /**
+   * Sets if animals should be allowed to spawn.
+   *
+   * @param allowAnimals the new value
+   */
   public void setAllowAnimals(boolean allowAnimals) {
     this.logger.debug(String.format("Setting allowAnimals to %b for %s.", allowAnimals, this.worldName));
     this.allowAnimals = allowAnimals;
     if (world != null) world.setSpawnFlags(allowMonsters, allowAnimals);
   }
-  
+
+  /**
+   * Sets if monsters should be allowed to spawn.
+   *
+   * @param allowMonsters the new value
+   */
   public void setAllowMonsters(boolean allowMonsters) {
     this.logger.debug(String.format("Setting allowMonsters to %b for %s.", allowMonsters, this.worldName));
     this.allowMonsters = allowMonsters;
     if (world != null) world.setSpawnFlags(allowMonsters, allowAnimals);
   }
-  
+
+  /**
+   * Sets the difficulty of the world.
+   *
+   * @param difficulty the new value.
+   */
   public void setDifficulty(Difficulty difficulty) {
     if (difficulty == null) throw new IllegalArgumentException("Difficulty can not be null!");
     this.logger.debug(String.format("Setting difficulty to %s for %s.", difficulty.name(), this.worldName));
     this.difficulty = difficulty;
     if (world != null) world.setDifficulty(difficulty);
   }
-  
+
+  /**
+   * Sets if this world should be automatically loaded when the plugin starts.
+   *
+   * @param enabled the new value
+   */
   public void setEnabled(boolean enabled) {
     this.logger.debug(String.format("Setting enabled to %s for %s.", enabled, this.worldName));
     this.enabled = enabled;
   }
-  
+
+  /**
+   * Sets the environment of the world.
+   *
+   * @param environment the new value
+   */
   public void setEnvironment(Environment environment) {
     if (environment == null) throw new IllegalArgumentException("Environment can not be null!");
     this.logger.debug(String.format("Setting environment to %b for %s.", environment, this.worldName));
     if (world != null) this.environment = environment;
   }
-  
+
+  /**
+   * Sets the game mode of the world.
+   *
+   * @param gameMode the new value
+   */
   public void setGameMode(GameMode gameMode) {
     if (environment == null) throw new IllegalArgumentException("GameMode can not be null!");
     this.logger.debug(String.format("Setting gameMode to %s for %s.", gameMode.name(), this.worldName));
     this.gameMode = gameMode;
     this.applyGameMode();
   }
-  
+
+  /**
+   * Sets if this world should generate structures.
+   *
+   * @param generateStructures the new value
+   */
   public void setGenerateStructures(boolean generateStructures) {
     this.logger.debug(String.format("Setting generateStructures to %s for %s.", generateStructures, this.worldName));
     this.setGenerateStructures(generateStructures);
   }
-  
+
+  /**
+   * Sets the generator id.
+   *
+   * @param generatorID the new value
+   */
   public void setGeneratorID(String generatorID) {
     this.logger.debug(String.format("Setting generatorID to %s for %s.", generatorID, this.worldName));
     this.generatorID = generatorID;
   }
-  
+
+  /**
+   * Sets the generator plugin name.
+   *
+   * @param generatorPlugin the new value
+   */
   public void setGeneratorPluginName(Plugin generatorPlugin) {
     if (generatorPlugin == null) throw new IllegalArgumentException("GeneratorPlugin can not be null!");
     this.logger.debug(String.format("Setting generatorPluginName to %s for %s.", generatorPlugin.getName(), this.worldName));
     this.generatorPluginName = generatorPlugin.getName();
   }
-  
+
+  /**
+   * Sets the generator plugin name.
+   *
+   * @param generatorPluginName the new value
+   */
   public void setGeneratorPluginName(String generatorPluginName) {
     if (generatorPluginName == null) throw new IllegalArgumentException("GeneratorPluginName can not be null!");
     this.logger.debug(String.format("Setting generatorPluginName to %s for %s.", generatorPluginName, this.worldName));
     this.generatorPluginName = generatorPluginName;
   }
-  
+
+  /**
+   * Sets if chat in this world should be isolated.
+   *
+   * @param isolatedChat the new value
+   */
   public void setIsolatedChat(boolean isolatedChat) {
     this.logger.debug(String.format("Setting isolatedChat to %b for %s.", isolatedChat, this.worldName));
     this.isolatedChat = isolatedChat;
   }
-  
+
+  /**
+   * Sets if we should keep the spawn of the world in memory.
+   *
+   * @param keepSpawnInMemory the new value
+   */
   public void setKeepSpawnInMemory(boolean keepSpawnInMemory) {
     this.logger.debug(String.format("Setting keepSpawnInMemory to %b for %s.", keepSpawnInMemory, this.worldName));
     this.keepSpawnInMemory = keepSpawnInMemory;
     if (world != null) world.setKeepSpawnInMemory(keepSpawnInMemory);
   }
-  
-  public void unregisterEvents() {
-    this.logger.debug(String.format("Unregistering events for %s.", this.worldName));
-    this.listening = false;
-  }
-  
+
+  /**
+   * Sets if PvP should be allowed in this world.
+   *
+   * @param pvp the new value
+   */
   public void setPVP(boolean pvp) {
     this.logger.debug(String.format("Setting pvp to %b for %s.", pvp, this.worldName));
     this.pvp = pvp;
     if (world != null) world.setPVP(pvp);
   }
-  
+
+  /**
+   * Sets the seed of the world.
+   *
+   * @param seed the new value
+   */
   public void setSeed(long seed) {
     if (seed != 0) throw new IllegalStateException("You may not change the seed of a world.");
     this.logger.debug(String.format("Setting seed to %d for %s.", seed, this.worldName));
     this.seed = seed;
   }
-  
+
+  /**
+   * Sets a reference to a loaded world.
+   *
+   * @param world the new value
+   */
   public void setWorld(org.bukkit.World world) {
     if (world != null) throw new IllegalStateException("You may not change an existing world reference.");
     this.logger.debug(String.format("Setting world reference for %s.", this.worldName));
     this.world = world;
   }
-  
+
+  /**
+   * Sets the type of world.
+   *
+   * @param worldType the new value
+   */
   public void setWorldType(WorldType worldType) {
     if (world != null) throw new IllegalStateException("You may not change the type of a world which is loaded.");
     this.logger.debug(String.format("Setting worldType to %s for %s.", worldType.name(), this.worldName));
     this.worldType = worldType;
   }
 
+  /**
+   * Unload a world and teleport players out if they are preventing the unload.
+   */
   public void unload() {
     if (world == null) throw new IllegalStateException("You may not unload a world which is not loaded.");
     if (isMainWorld()) throw new IllegalStateException("You may not unload the main world.");
@@ -343,7 +500,20 @@ public class World extends Localised implements Serializable, Listener {
     this.removePlayers();
     this.plugin.getServer().unloadWorld(world, true);
   }
-  
+
+  /**
+   * Unregister events.
+   */
+  public void unregisterEvents() {
+    this.logger.debug(String.format("Unregistering events for %s.", this.worldName));
+    this.listening = false;
+  }
+
+  /**
+   * Apply the world's game mode to all players in the world.
+   *
+   * @return the int
+   */
   private int applyGameMode() {
     int i = 0;
     for (Player player : world.getPlayers()) {
@@ -354,17 +524,31 @@ public class World extends Localised implements Serializable, Listener {
     return i;
   }
 
+  /**
+   * Apply the world's game mode to a specific player.
+   *
+   * @param player the player
+   */
   private void applyGameMode(Player player) {
     if (difficulty == null) throw new IllegalArgumentException("Player can not be null!");
     player.setGameMode(gameMode);
   }
 
+  /**
+   * Check if world is loaded.
+   */
   private void checkIfWorldIsLoaded() {
     for (org.bukkit.World world : this.plugin.getServer().getWorlds()) {
       if (world.getName().equalsIgnoreCase(worldName)) this.world = world;
     }
   }
+  
 
+  /**
+   * Gets the custom chunk generator.
+   *
+   * @return the custom chunk generator
+   */
   private ChunkGenerator getCustomChunkGenerator() {
     if (generatorPluginName != null) {
       Plugin generatorPlugin = this.plugin.getServer().getPluginManager().getPlugin(generatorPluginName);
@@ -373,14 +557,25 @@ public class World extends Localised implements Serializable, Listener {
       } else {
         throw new IllegalStateException("Unable to load world generation plugin!");
       }
-    } 
+    }
     return null;
   }
+  
 
+  /**
+   * Gets the main world spawn.
+   *
+   * @return the main world spawn
+   */
   private Location getMainWorldSpawn() {
     return plugin.getServer().getWorlds().get(0).getSpawnLocation();
   }
 
+  /**
+   * Gets the world creator.
+   *
+   * @return the world creator
+   */
   private WorldCreator getWorldCreator() {
     WorldCreator worldCreator = new WorldCreator(worldName);
     worldCreator.environment(environment);
@@ -392,6 +587,11 @@ public class World extends Localised implements Serializable, Listener {
     return worldCreator;
   }
 
+  /**
+   * Removes players from this world by teleporting them to the main world.
+   *
+   * @return the int
+   */
   private int removePlayers() {
     int i = 0;
     for (Player player : world.getPlayers()) {
@@ -401,5 +601,5 @@ public class World extends Localised implements Serializable, Listener {
     this.logger.debug(String.format("Removed %d players from %s.", i, this.worldName));
     return i;
   }
-  
+
 }
--- src/main/java/name/richardson/james/bukkit/dimensiondoor/WorldManager.java
+++ src/main/java/name/richardson/james/bukkit/dimensiondoor/WorldManager.java
@@ -1,49 +1,76 @@
 package name.richardson.james.bukkit.dimensiondoor;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerChatEvent;
 import org.bukkit.event.world.WorldInitEvent;
+
+import name.richardson.james.bukkit.utilities.internals.Logger;
 
 public class WorldManager implements Listener {
 
+  /** The logger for this class. */
+  private final Logger logger = new Logger(this.getClass());
+  
+  /** The storage backing the WorldManager. */
   private final WorldConfiguration storage;
   
-  private final Set<String> isolatedWorlds = new HashSet<String>(8);
-  
+  /** A collection of configured worlds. */
   private final Map<String, World> worlds;
 
+  /** The plugin. */
   private final DimensionDoor plugin;
   
+  /**
+   * Instantiates a new world manager.
+   *
+   * @param plugin the plugin
+   * @throws IOException Signals that an I/O exception has occurred loading the configuration
+   */
   public WorldManager(DimensionDoor plugin) throws IOException {
+    this.logger.debug("Initalising world manager.");
     this.storage = new WorldConfiguration(plugin);
     this.plugin = plugin;
     this.worlds = this.storage.getWorlds();
+    int i = 0;
     for (World world : this.worlds.values()) {
       if (world.isEnabled()) world.load();
+      i++;
     }
+    this.logger.debug(String.format("Enabled %d of %d configured worlds.", i, this.worlds.size()));
   }
   
+  /**
+   * When a world is initialised, check that it has been configured, if not create one.
+   *
+   * @param event the event
+   */
   @EventHandler(priority=EventPriority.MONITOR, ignoreCancelled=true) 
   public void onWorldInit(WorldInitEvent event) {
+    this.logger.debug(String.format("A world called %s has initalised.", event.getWorld().getName()));
     if (worlds.containsKey(event.getWorld().getName())) return;
+    this.logger.debug(String.format("%s has not been configured by DimensionDoor.", event.getWorld().getName()));
     this.worlds.put(event.getWorld().getName(), new World(this.plugin, event.getWorld()));
   }
   
+  /**
+   * Gets a configured world.
+   *
+   * @param name the name
+   * @return the world
+   */
   public World getWorld(String name) {
     return worlds.get(name);
   }
   
+  /**
+   * Removes a configured world.
+   *
+   * @param name the name
+   */
   public void removeWorld(String name) {
     World world = this.getWorld(name);
     if (world != null) {
@@ -54,17 +81,18 @@ public class WorldManager implements Listener {
     }
   }
   
-  public void refresh() {
-    this.isolatedWorlds.clear();
-    for (World world : this.worlds.values()) {
-      if (world.isChatIsolated()) this.isolatedWorlds.add(world.getName());
-    }
-  }
-  
+  /**
+   * Save all worlds to the backing storage.
+   */
   public void save() {
     storage.setWorlds(worlds);
   }
   
+  /**
+   * Get the number of configured worlds.
+   *
+   * @return the int
+   */
   public int configuredWorldCount() {
     return this.worlds.size();
   }
