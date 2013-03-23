55c78fa834eae55f0aaa4f91d13105f81a70be25
James Richardson <me+github@james.richardson.name>
2013-03-23 13:10:12 +0000

    prevent prompt when setting default packs

--- src/main/java/name/richardson/james/bukkit/dimensiondoor/SwitchTexturePackTask.java
+++ src/main/java/name/richardson/james/bukkit/dimensiondoor/SwitchTexturePackTask.java
@@ -41,6 +41,7 @@ public class SwitchTexturePackTask implements Runnable {
   }
   
   public void run() {
+    if (texturePack.equalsIgnoreCase("Default")) return;
     for (Player player : players) {
       player.setTexturePack(texturePack);
     }
